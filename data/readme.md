f6e16f6c247af4b71ee6121a5ea30017a594cb66
Michiel <m.a.noback@pl.hanze.nl>
2025-04-17 09:00:20 +0200

    work on analysis pipeline

--- data/readme.md
+++ data/readme.md
@@ -1,10 +1,25 @@
-# Data source
-downloaded from
+# File descriptions
 
-https://ironman.tenwisedev.nl/public/abstract_set1_michiel.txt
- 
-https://ironman.tenwisedev.nl/public/abstract_set2_michiel.txt
-
-on feb 6, 2025
-
-It was undisclosed which category the abstracts belonged to.
+- ActiveSubstanceExport_24-03-2025.xlsx
+    > Downloaded from https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/start/screen/active-substances. (Plants > Pesticides > EU pesticides database > active substances) as is a an excel (`ActiveSubstanceExport_24-03-2025.xlsx`) with 1465 substances, all with scientific name only, it seems.
+- abstract_set1.txt
+    > downloaded from https://ironman.tenwisedev.nl/public/abstract_set1_michiel.txt. on feb 6, 2025 It was undisclosed which category the abstracts belonged to, but it is quite obvious that set1 contains pesticide-related abstracts. 
+- abstract_set2.txt
+    > downloaded from https://ironman.tenwisedev.nl/public/abstract_set2_michiel.txt. on feb 6, 2025 It was undisclosed which category the abstracts belonged to, but it is quite obvious that set2 contains disease-related abstracts
+- abstracts_2025_04_09.csv
+    > a single-day PubMed production of published papers
+- common_names.csv
+    > Downloaded from http://www.bcpcpesticidecompendium.org/index_cn_frame.html and parsed to simple text file on march 31, 2025
+- curated_pesticides.txt
+    > pubmed IDs for papers that were labeled by Wynand to belong to one of five groups: human, eco-animal, insect, animal, other. This file (obtained on april 10, 2025 from https://ironman.tenwisedev.nl/public//curated_pesticides.txt) contains 164 entries
+- drugLib_raw.tsv
+    > test file for Transformer experimentation
+- parse_common_names.py
+    > To parse data from http://www.bcpcpesticidecompendium.org/index_cn_frame.html
+- pesticide_classes.csv
+    > a small text file containing the general classes for pesticied (e.g. herbicides, fungicides etc.)
+- tf_idf_keyword_extraction_test.csv
+    > results of an experiment to generate keywords using a tf-idf embedding. See log for details 
+- tf_idf_vocabulary_size_test.csv
+    > It was investigated how small a TF-IDF can be for finding a correct recommendations using nearest neighbour search
+    
--- entrez_utils.py
+++ entrez_utils.py
@@ -6,41 +6,38 @@ def init(api_key, email):
     Entrez.email = email
 
 
-#adapted from https://stackoverflow.com/questions/73000220/extracting-multiple-abstracts-from-pubmed-from-external-pubmed-ids-list-with-bio
-def fetch_abstracts(pub_ids,
-                    retmax=1000, 
-                    output_file='abstracts.csv', 
-                    append=False):
-    # Make sure requests to NCBI are not too big
-    # Iterate with steps of `retmax`
-    for i in range(0, len(pub_ids), retmax):
-        j = i + retmax
-        if j >= len(pub_ids):
-            j = len(pub_ids)
+def query_pubmed_by_date(date=None, retmax=5000):
+    """
+    Fetches Pubmed papers by date.
+    :param date: Date in YYYY/MM/DD format. Defaults to yesterday.
+    :param retmax: Number of records to fetch.
+    :return: List of Pubmed IDs.
+    """
+    if date is None:
+        date = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y/%m/%d')
+    query = f'{date}[dp]'
 
-        print(f"Fetching abstracts from {i} to {j}.")
-        id_string = ','.join(pub_ids[i:j])
-        handle = Entrez.efetch(db="pubmed", id=id_string, rettype="xml", retmode="text", retmax=retmax)
-        
-        records = Entrez.read(handle)
-        mode = 'a' if append else 'w'
-        with open(output_file, mode, newline='') as csvfile:
-            csvfile.write(f'mid\ttitle\tabstract\n')
-            for pubmed_article in records['PubmedArticle']:
-                #print(pubmed_article['MedlineCitation'].keys())
-                pub_id = pubmed_article['MedlineCitation']['PMID']
-                title = pubmed_article['MedlineCitation']['Article']['ArticleTitle']
-                abstract = 'NA'
-                if 'Abstract' in pubmed_article['MedlineCitation']['Article'].keys():
-                    abstract = pubmed_article['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
-                #print(f'PUBMED_ID={pub_id}, TITLE={title}\n')
-                csvfile.write(f'{pub_id}\t{title}\t{abstract}\n')
+    print(f"Querying Pubmed with: {query}")
+
+    # fetch papers by term
+    stream = Entrez.esearch(
+        db="pubmed", term=query, retmax=retmax, usehistory="y"
+    )
+    record = Entrez.read(stream)
+    return record["IdList"]
 
 def query_pubmed(keywords_list, 
                  start_date="", 
                  end_date=datetime.datetime.today().strftime('%Y/%m/%d'), 
                  retmax=1000):
-    
+    """
+    Fetches Pubmed papers by keywords and date.
+    :param keywords_list: List of keywords to search for.
+    :param start_date: Start date in YYYY/MM/DD format.
+    :param end_date: End date in YYYY/MM/DD format. Defaults to today.
+    :param retmax: Number of records to fetch.
+    :return: List of Pubmed IDs.
+    """
     query = " OR ".join(keywords_list)
     query = f"({query})"
 
@@ -55,5 +52,50 @@ def query_pubmed(keywords_list,
     )
     record = Entrez.read(stream)
     return record["IdList"]
+
+
+
+#adapted from https://stackoverflow.com/questions/73000220/extracting-multiple-abstracts-from-pubmed-from-external-pubmed-ids-list-with-bio
+def fetch_abstracts(pub_ids,
+                    chunksize=1000, 
+                    output_file='abstracts.csv', 
+                    append=False):
+    """
+    Fetches abstracts from Pubmed for a list of Pubmed IDs.
+    :param pub_ids: List of Pubmed IDs.
+    :param chunksize: Number of records to fetch at once.
+    :param output_file: Output file name.
+    :param append: Whether to append to the output file or overwrite it.
+    :return: None
+    """
+    
+    # Make sure requests to NCBI are not too big
+    # Iterate with steps of `chunksize`
+    for i in range(0, len(pub_ids), chunksize):
+        j = i + chunksize
+        if j >= len(pub_ids):
+            j = len(pub_ids)
+
+        print(f"Fetching abstracts: from {i} to {j}.")
+        if isinstance(pub_ids[0], int):
+            pub_ids = [str(x) for x in pub_ids]
+        id_string = ','.join(pub_ids[i:j])
+        #print(f'ID_STRING={id_string}')
+
+        handle = Entrez.efetch(db="pubmed", id=id_string, rettype="xml", retmode="text", retmax=chunksize)
+        
+        records = Entrez.read(handle)
+        mode = 'a' if append else 'w'
+        with open(output_file, mode, newline='') as csvfile:
+            csvfile.write(f'pmid\ttitle\tabstract\n')
+            for pubmed_article in records['PubmedArticle']:
+                #print(pubmed_article['MedlineCitation'].keys())
+                pub_id = pubmed_article['MedlineCitation']['PMID']
+                title = pubmed_article['MedlineCitation']['Article']['ArticleTitle']
+                abstract = 'NA'
+                if 'Abstract' in pubmed_article['MedlineCitation']['Article'].keys():
+                    abstract = pubmed_article['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
+                #print(f'PUBMED_ID={pub_id}, TITLE={title}\n')
+                csvfile.write(f'{pub_id}\t{title}\t{abstract}\n')
 
 
